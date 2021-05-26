5638911f185eae067e3006afeb84358501b6e3c6
Ssegura09 <stephaniemsegura09@gmail.com>
2021-05-25 21:52:54 -0500

    SS: Scanner CLI App done

---
+++ src/main/java/com/qa/main/Calculator.java
@@ -0,0 +1,24 @@
+package com.qa.main;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		return a + b;
+
+	}
+
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multi(int a, int b) {
+		return a * b;
+
+	}
+	
+	public static int divi(int a, int b) {
+		return a / b;
+
+	}
+
+}
